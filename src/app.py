4bb1688cbf2dc1000afe6f03ffa282b0297e6e3c
Irfan Shaikh <irfan.shaikh@ibm.com>
2024-10-20 13:33:36 +0530

    updating python packages in pyfile

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template
-from flask_wtf.csrf import CSRFProtect, csrf_exempt
+from flask_wtf.csrf import CSRFProtect
 import socket
 
 app = Flask(__name__)
@@ -7,9 +7,6 @@ app = Flask(__name__)
 # Initialize CSRF protection
 csrf = CSRFProtect(app)
 
-# Disable CSRF protection globally
-csrf._disable_on_request = True
-    
 # Function to fetch hostname and IP address
 def fetchdetails():
     try:
@@ -17,13 +14,14 @@ def fetchdetails():
         ip_address = socket.gethostbyname(hostname)
         return str(hostname), str(ip_address)
     except socket.error:
-        return "Unknown", "Unknown"    
+        return "Unknown", "Unknown"
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/health")
+@csrf.exempt  # Disable CSRF protection on this route
 def health():
     return jsonify(status="up")
 
@@ -34,4 +32,3 @@ def details():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
-
